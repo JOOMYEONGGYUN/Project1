d7c769d0b449c626189e9b2cd7edbff1fd5cc349
JOOMYEONGGYUN <106478922+JOOMYEONGGYUN@users.noreply.github.com>
2023-05-08 10:41:41 +0900

    Page

--- page.h
+++ page.h
@@ -1,6 +1,14 @@
 class Page {
     public:
-
+        Page(int x, int y, int width, int height, int id, char content): x(x), y(y), width(width), height(height), id(id), content(content) {}
+        Page(char content): content(content){}
+        Page(){};
+        int getx(){return x;}
+        int gety(){return y;}
+        int getw(){return width;}
+        int geth(){return height;}
+        int get_id(){return id;}
+        char getc(){return content;}
     private:
         int x, y; // position of the page on the board
         int width, height; // width and height of the page 
